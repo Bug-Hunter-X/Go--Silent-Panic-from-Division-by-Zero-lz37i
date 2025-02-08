65eaca6221175e5b92f82b083788716cc531cdda
BugHunter-X <askzenai@gmail.com>
2025-02-08 07:56:05 +0100

    Add bug.go file

---
+++ bug.go
@@ -0,0 +1,15 @@
+func myFunc(a, b int) (int, error) {
+  if a == 0 {
+    return 0, fmt.Errorf("a cannot be zero")
+  }
+  return a / b, nil
+}
+
+func main() {
+  result, err := myFunc(10, 0)
+  if err != nil {
+    fmt.Println("Error:", err)
+  } else {
+    fmt.Println("Result:", result)
+  }
+}
